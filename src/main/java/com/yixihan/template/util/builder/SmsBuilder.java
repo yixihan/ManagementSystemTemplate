9c4defffba2122941e327c7ab103b66cb6e874e4
Yves Zeng <yves.zeng@bytesforce.com>
2024-06-03 15:42:59 +0800

    smsBuilder 完善1

--- src/main/java/com/yixihan/template/util/builder/SmsBuilder.java
+++ src/main/java/com/yixihan/template/util/builder/SmsBuilder.java
@@ -1,7 +1,20 @@
 package com.yixihan.template.util.builder;
 
 import cn.hutool.core.util.ObjUtil;
+import cn.hutool.extra.spring.SpringUtil;
+import cn.hutool.json.JSONUtil;
+import com.tencentcloudapi.common.Credential;
+import com.tencentcloudapi.common.exception.TencentCloudSDKException;
+import com.tencentcloudapi.common.profile.ClientProfile;
+import com.tencentcloudapi.common.profile.HttpProfile;
+import com.tencentcloudapi.sms.v20210111.SmsClient;
+import com.tencentcloudapi.sms.v20210111.models.SendSmsRequest;
+import com.tencentcloudapi.sms.v20210111.models.SendSmsResponse;
+import com.tencentcloudapi.sms.v20210111.models.SendStatus;
+import com.yixihan.template.config.third.SmsConfig;
+import com.yixihan.template.enums.ExceptionEnums;
 import com.yixihan.template.enums.SmsSourceEnums;
+import com.yixihan.template.exception.BizException;
 import com.yixihan.template.util.Assert;
 import lombok.extern.slf4j.Slf4j;
 
@@ -21,7 +34,7 @@ public class SmsBuilder {
 
     private String templateId;
 
-    private Map<String, Object> params;
+    private Map<String, String> params;
 
     private String source;
 
@@ -43,7 +56,7 @@ public class SmsBuilder {
         return this;
     }
 
-    public SmsBuilder params(Map<String, Object> params) {
+    public SmsBuilder params(Map<String, String> params) {
         this.params = params;
         return this;
     }
@@ -67,6 +80,83 @@ public class SmsBuilder {
         Assert.notBlank(templateId);
         Assert.notBlank(source);
 
-        // todo
+
+        if (SmsSourceEnums.TENCENT.getValue().equals(source)) {
+            tencentSend();
+        } else if (SmsSourceEnums.AL.getValue().equals(source)) {
+            alSend();
+        } else {
+            // todo
+        }
+    }
+
+    private void alSend() {
+        SmsConfig smsConfig = SpringUtil.getBean(SmsConfig.class);
+        try {
+            com.aliyun.dysmsapi20170525.Client client = createClient(smsConfig);
+            com.aliyun.dysmsapi20170525.models.SendSmsRequest sendSmsRequest = new com.aliyun.dysmsapi20170525.models.SendSmsRequest()
+                    // 发送手机号
+                    .setPhoneNumbers(toMobile)
+                    // 短信签名
+                    .setSignName(smsConfig.getSignName())
+                    // 模板 code
+                    .setTemplateCode(templateId)
+                    // 模板参数
+                    .setTemplateParam(JSONUtil.toJsonStr(params));
+            com.aliyun.dysmsapi20170525.models.SendSmsResponse sendResp = client.sendSmsWithOptions(sendSmsRequest, new com.aliyun.teautil.models.RuntimeOptions());
+            String code = sendResp.body.code;
+            if (!com.aliyun.teautil.Common.equalString(code, "OK")) {
+                log.error("短信服务出现异常: {}", sendResp.body.message);
+                throw new BizException(ExceptionEnums.SMS_SEND_ERR);
+            }
+        } catch (Exception e) {
+            log.error("短信服务出现异常!", e);
+            throw new BizException(ExceptionEnums.SMS_SEND_ERR);
+        }
+    }
+
+    public static com.aliyun.dysmsapi20170525.Client createClient(SmsConfig smsConfig) throws Exception {
+        com.aliyun.teaopenapi.models.Config config = new com.aliyun.teaopenapi.models.Config()
+                .setAccessKeyId(smsConfig.getSecretId())
+                .setAccessKeySecret(smsConfig.getSecretKey());
+        // Endpoint 请参考 https://api.aliyun.com/product/Dysmsapi
+        config.endpoint = "dysmsapi.aliyuncs.com";
+        return new com.aliyun.dysmsapi20170525.Client(config);
+    }
+
+    public void tencentSend() {
+        try {
+            SmsConfig smsConfig = SpringUtil.getBean(SmsConfig.class);
+            SmsClient client = getSmsClient(smsConfig);
+            // 实例化一个请求对象,每个接口都会对应一个request对象
+            SendSmsRequest req = new SendSmsRequest();
+            String[] phoneNumberSet1 = {toMobile};
+            req.setPhoneNumberSet(phoneNumberSet1);
+            req.setSmsSdkAppId(smsConfig.getSmsSdkAppId());
+            req.setSignName(smsConfig.getSignName());
+            req.setTemplateId(templateId);
+            String[] templateParamSet1 = params.values().toArray(new String[0]);
+            req.setTemplateParamSet(templateParamSet1);
+            // 返回的resp是一个SendSmsResponse的实例，与请求对象对应
+            SendSmsResponse resp = client.SendSms(req);
+            SendStatus sendStatus = resp.getSendStatusSet()[0];
+
+            Assert.isTrue("Ok".equals(sendStatus.getCode()), ExceptionEnums.SMS_SEND_ERR);
+        } catch (TencentCloudSDKException e) {
+            log.error("短信服务出现异常!", e);
+            throw new BizException(ExceptionEnums.SMS_SEND_ERR);
+        }
+    }
+
+    private static SmsClient getSmsClient(SmsConfig smsConfig) {
+        Credential cred = new Credential(smsConfig.getSecretId(), smsConfig.getSecretKey());
+        // 实例化一个http选项，可选的，没有特殊需求可以跳过
+        HttpProfile httpProfile = new HttpProfile();
+        httpProfile.setEndpoint("sms.tencentcloudapi.com");
+        // 实例化一个client选项，可选的，没有特殊需求可以跳过
+        ClientProfile clientProfile = new ClientProfile();
+        clientProfile.setHttpProfile(httpProfile);
+        // 实例化要请求产品的client对象,clientProfile是可选的
+        return new SmsClient(cred, "ap-guangzhou", clientProfile);
     }
 }
